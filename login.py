71bdeac4e223a7d8fc8930457b679b6a51894851
jjpatric <jjpatric@ualberta.ca>
2022-01-24 22:08:46 -0500

    Actually use cookies

--- login.py
+++ login.py
@@ -13,7 +13,19 @@ s = cgi.FieldStorage()
 username = s.getfirst("username")
 password = s.getfirst("password")
 
+cookie = SimpleCookie(os.environ["HTTP_COOKIE"])
+cookie_username = None
+cookie_password = None
+if cookie.get("username"):
+    cookie_username = cookie.get("username").value
+if cookie.get("password"):
+    cookie_password = cookie.get("password").value
 
+cookie_auth = cookie_username == secret.username and cookie_password == secret.password
+
+if cookie_auth:
+    username = cookie_username
+    password = cookie_password
 
 print("Content-Type: text/html")
 
